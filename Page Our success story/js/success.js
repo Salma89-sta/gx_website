dd82acde642b74b4004830e0166d3efef60fd9a3
Salma89-sta <75533821+Salma89-sta@users.noreply.github.com>
2023-11-08 14:22:22 +0200

    gx

---
+++ Page Our success story/js/success.js
@@ -0,0 +1,108 @@
+var img=Array.from( document.querySelectorAll(".allImages"));
+
+var lightboxcontainer=document.getElementById("lightboxcontainer");
+var lightboxcontaineritem=document.getElementById("lightboxcontaineritem");
+
+var closebtn=document.getElementById("close");
+var nextbtn=document.getElementById("next");
+var prevbtn=document.getElementById("prev");
+
+var currentindex=0;
+
+var replace=['img/1d.jpg','img/2d.jpg','img/3d.jpg','img/4d.jpg','img/5d.png','img/6d.png','img/7d.jpg','img/8d.jpg','img/9d.jpg','img/10d.jpg','img/11d.jpg']
+
+ 
+
+for(var i=0;i<img.length;i++)
+{
+   img[i].addEventListener("click",function(eventInfo){
+       
+       currentindex=img.indexOf(eventInfo.target);
+       var src= eventInfo.target.getAttribute("src");
+       
+       console.log(currentindex)
+
+
+       lightboxcontaineritem.style.backgroundImage="url("+replace[currentindex-1]+")";
+       lightboxcontainer.style.display="flex";
+
+   })
+
+   
+}
+
+
+
+closebtn.addEventListener("click",function()
+{
+  lightboxcontainer.style.display="none ";    
+})
+
+
+
+function nextslide()
+{
+currentindex++;
+if(currentindex==replace.length)
+{
+    currentindex=0; 
+}
+var src=img[currentindex].getAttribute("src");
+lightboxcontaineritem.style.backgroundImage="url("+replace[currentindex-1]+")";
+}
+nextbtn.addEventListener("click",nextslide)
+
+
+ 
+
+
+function prevslide()
+{
+currentindex--;
+if(currentindex<0)
+{
+    currentindex=replace.length-1 ; 
+}
+var src=img[currentindex].getAttribute("src");
+lightboxcontaineritem.style.backgroundImage="url("+replace[currentindex-1]+")";
+}
+
+prevbtn.addEventListener("click",prevslide)
+
+
+
+
+
+
+
+document.addEventListener("keydown",function(eventInfo){
+
+    if(eventInfo.keyCode==39)
+    {
+    nextslide();    
+    }
+        
+    })
+    
+    document.addEventListener("keydown",function(eventInfo){
+    
+        if(eventInfo.keyCode==37 )
+        {
+        prevslide();    
+        }
+            
+        })
+  
+        document.addEventListener("keydown",function(eventInfo){
+    
+          if(eventInfo.keyCode==27 )
+          {
+            lightboxcontainer.style.display="none ";    
+  
+          }
+              
+          })
+          
+      
+        
+    
---
+++ js/js.js
@@ -0,0 +1,59 @@
+
+
+ 
+  
+
+
+const pics =[
+    "img/ai-cloud-concept-with-robot-arm 2.png ",
+    "img/anniversarry copy 1.png", 
+    "img/Group 87.png",
+    "img/Group 88.png",
+    "img/10d 2.png",
+    "img/6d 1.png",
+     "img/BID 2012 2.png",
+ 
+ ];
+
+
+setInterval(function(){
+
+    var a =Math.floor(Math.random()*pics.length)
+
+    cartona =  ` 
+    
+          <img src="${pics[a]}">
+             
+                 `
+        
+        document.getElementById('inside-img').innerHTML=cartona
+    
+
+},2000)
+
+
+
+
+
+
+    // scroll to top button
+    function scrollToTop() {
+        window.scroll({
+          top: 0,
+          behavior: 'smooth'
+        });
+      }
+      
+      window.addEventListener('scroll', function() {
+        const scrollToTopButton = document.getElementById('scrollToTop');
+        if (scrollY >= 100) { 
+          scrollToTopButton.style.display = 'block';
+        } else {
+          scrollToTopButton.style.display = 'none';
+        }
+      });
+      
+      const scrollToTopButton = document.getElementById('scrollToTop');
+      scrollToTopButton.addEventListener('click', scrollToTop);
+      
+      
